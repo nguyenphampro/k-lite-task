62cf936909ddbff3ab04c76aa43367e531fcea0d
DESKTOP-F9RP5JM\NguyenPham <baonguyenyam@gmail.com>
2017-04-16 15:02:29 +0700

    up

--- src/scripts/fixed-header.js
+++ src/scripts/fixed-header.js
@@ -1,20 +1,20 @@
-function setHeader(elm) {
-    if (elm >= 50) {
-        $("header").removeClass('transparent');
-    } else {
-        $("header").addClass('transparent');
-    }
-}
+// function setHeader(elm) {
+//     if (elm >= 50) {
+//         $("header").removeClass('transparent');
+//     } else {
+//         $("header").addClass('transparent');
+//     }
+// }
 
-$(document).ready(function() {
-    $("header").addClass('transparent active');
+// $(document).ready(function() {
+//     $("header").addClass('transparent active');
 
-    if ($(window).scrollTop() >= 50) {
-        setHeader($(window).scrollTop());
-    }
-});
+//     if ($(window).scrollTop() >= 50) {
+//         setHeader($(window).scrollTop());
+//     }
+// });
 
-// Fixed Header
-$(window).scroll(function() {
-    setHeader($(document).scrollTop());
-});
+// // Fixed Header
+// $(window).scroll(function() {
+//     setHeader($(document).scrollTop());
+// });
