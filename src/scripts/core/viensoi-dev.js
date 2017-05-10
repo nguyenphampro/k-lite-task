963f360d47c9955bcaa1cabf9beb8370fbd17c0b
DESKTOP-F9RP5JM\NguyenPham <baonguyenyam@gmail.com>
2017-05-10 21:34:53 +0700

    Finished Dev tools

--- src/scripts/core/viensoi-dev.js
+++ src/scripts/core/viensoi-dev.js
@@ -14,18 +14,18 @@ if (VIENSOI_APP.DEV_MODE) {
             var $el = this.find(opt.handle);
         }
 
-        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+        return $el.find('.header-devtools').css('cursor', opt.cursor).on("mousedown", function(e) {
             if (opt.handle === "") {
-                var $drag = $(this).addClass('draggable');
+                var $drag = $(this).parent().addClass('draggable');
             } else {
-                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+                var $drag = $(this).parent().addClass('active-handle').parent().addClass('draggable');
             }
             var z_idx = $drag.css('z-index'),
                 drg_h = $drag.outerHeight(),
                 drg_w = $drag.outerWidth(),
                 pos_y = $drag.offset().top + drg_h - e.pageY,
                 pos_x = $drag.offset().left + drg_w - e.pageX;
-            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+            $drag.css('z-index', 99999).parents().on("mousemove", function(e) {
                 $('.draggable').offset({
                     top: e.pageY + pos_y - drg_h,
                     left: e.pageX + pos_x - drg_w
@@ -46,3 +46,19 @@ if (VIENSOI_APP.DEV_MODE) {
 })(jQuery);
 
 $('.devtools').drags();
+
+$(document).ready(function() {
+    if ($('.viensoi-develop .devtools').length) {
+        var devtls = $('.viensoi-develop .devtools').find('.body-devtools button')
+        devtls.click(function() {
+            if ($(this).attr('data-click-state') == 1) {
+                $(this).attr('data-click-state', 0)
+                $('body').find('.devtool-gird').remove()
+            } else {
+                $(this).attr('data-click-state', 1)
+                $('body').toggleClass('active')
+                $('body').append('<div class="devtool-gird"><div class="container d-flex"><div class="row d-flex align-items-stretch bd-highlight"><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div><div class="col d-flex align-items-stretch"><div class="item"></div></div></div></div></div>')
+            }
+        });
+    }
+});
